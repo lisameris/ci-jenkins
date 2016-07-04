798f0eec984e96713d13e0a579eb6b24ab9725d0
Lisa Hunt <L10NLass@gmail.com>
2016-07-04 13:16:26 -0400

    changed mother to momma

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-  res.send('hello mother');
+  res.send('hello momma');
 });
  
 app.listen(process.env.PORT || 5000);
